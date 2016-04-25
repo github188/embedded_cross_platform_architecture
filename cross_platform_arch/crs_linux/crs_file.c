26d756a9c9d4034d337ed10cb813870ba1c782ef
wangwenlong <interestingwwl@sina.com>
2016-04-25 22:12:48 +0800

    update linux crs_file.c

--- cross_platform_arch/crs_linux/crs_file.c
+++ cross_platform_arch/crs_linux/crs_file.c
@@ -4,6 +4,14 @@
 */
 
 #include "crs_types.h"
+#include "crs_mem.h"
+#include "crs_file.h"
+
+#include <fcntl>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #define STR_LEN 60
 /*
 	SEEK_SET,SEEK_CUR,SEEK_END三个宏定义是用于crs_file_seek函数的第三个参数whence的，表示偏移量的相对的位置
@@ -29,8 +37,11 @@
 		fail : 	
 */
 
-typedef struct crs_file_handler crs_file_handler_t;
- 
+struct dana_file_handler_s {
+    int fd;
+    file_info_t *finfo;
+};
+
  /*
 	function : 
 		文件打开模式			
@@ -90,14 +101,26 @@ typedef struct
 	return value : 
 		success :	返回文件的控制handle
 		fail : 	返回NULL
+		open ("helloq.txt", O_RDWR | O_CREAT , S_IRUSR | S_IWUSR)
 */
-extern crs_file_handler_t* crs_file_open(crs_file_handler_t *file, file_mode_e mode)
+extern crs_file_handler_t* crs_file_open(const int8_t *file_name, file_mode_e mode)
 {
+	crs_file_handler_t *file_handle = (crs_file_handler_t *)crs_malloc(sizeof(crs_file_handler_t));
+	crs_memcpy( file_handle -> finfo -> file_name, file_name, crs_strlen(file_name));
+	file_handle -> finfo -> file_name_len = crs_strlen(file_name);
 	switch (mode)
 	{
-	case fmode_r : fopen()
+	case fmode_r :
+			file_handle -> fd = open( file_name, O_RDONLY ,  S_IRUSR | S_IWUSR); break;
+			return file_handle;
 	case fmode_w:
+			file_handle -> fd = open(file_name, O_WRONLY , S_IRUSR | S_IWUSR); break;
+			return file_handle;
 	case fmode_a:
+			file_handle -> fd = open(file_name, O_APPEND | O_CREAT, S_IRUSR | S_IWUSR); break;
+			return file_handle;
+	default :
+			return NULL;
 	}
 }
 
@@ -112,7 +135,10 @@ extern crs_file_handler_t* crs_file_open(crs_file_handler_t *file, file_mode_e m
 		success : 返回 0，表示已经读取0个字节，返回正数，表示读取到的字节数
 		fail : 返回 -1：表示读取发生错误 
 */
-extern int32_t crs_file_read(crs_file_handler_t *file, int8_t *buf, uint32_t n);
+extern int32_t crs_file_read(crs_file_handler_t *file, int8_t *buf, uint32_t n)
+{
+	return read(file -> finfo -> file_name, buf, n);
+}
 
   /*
 	function :
@@ -125,7 +151,12 @@ extern int32_t crs_file_read(crs_file_handler_t *file, int8_t *buf, uint32_t n);
 		success : 返回值为0：表示没有写入数据， 返回值为正数：表示写入的字节数
  		fail : 返回值为-1：表示写入发生错误
 */
-extern int32_t crs_file_write(crs_file_handler_t *file, int8_t *buf, uint32_t n);
+extern int32_t crs_file_write(crs_file_handler_t *file, int8_t *buf, uint32_t n)
+{
+	return write ( file -> finfo -> file_name, buf, n);
+
+
+}
   /*
 	function :
 		获取文件的信息
@@ -133,10 +164,21 @@ extern int32_t crs_file_write(crs_file_handler_t *file, int8_t *buf, uint32_t n)
 		crs_file_handler_t *file : 文件控制handle
 		file_info_t *info : 描述文件的结构体
 	return value :
-		success : 将file指针指向的文件的相关的信息赋值到file_info结构体中,同时返回1
-		fail : 返回 0
+		success : 将file指针指向的文件的相关的信息赋值到file_info结构体中,同时返回0
+		fail : 返回 -1
 */
-extern int32_t crs_file_get_info(crs_file_handler_t *file, file_info_t *info);
+extern int32_t crs_file_get_info(crs_file_handler_t *file, file_info_t *finfo)
+{
+	file_info_t *finfo_t = crs_malloc( sizeof( file_info_t ));
+
+	if(NULL == file || NULL == file -> finfo)
+	{
+		crs_dbg("file info is NULL\r\n");
+		return -1;
+	}
+	crs_memcpy( finfo, file -> finfo);
+	return 0;
+}
  /*
 	function :
 		从whence处算起,移动文件读写指针到文件的离whence第pos个字节处
@@ -148,7 +190,10 @@ extern int32_t crs_file_get_info(crs_file_handler_t *file, file_info_t *info);
 		success : 文件指针指向
 		fail : 返回0
 */
-extern int32_t crs_file_seek(crs_file_handler_t *file, int32_t pos, int32_t whence);
+extern int32_t crs_file_seek(crs_file_handler_t *file, int32_t pos, int32_t whence)
+{
+	return lseek( file -> fd , pos, whence);
+}
 
   /*
 	function :
@@ -159,5 +204,14 @@ extern int32_t crs_file_seek(crs_file_handler_t *file, int32_t pos, int32_t when
 		success : 返回 1
 		fail : 返回 0
 */
-extern int32_t crs_file_close(crs_file_handler_t *file);
+extern int32_t crs_file_close(crs_file_handler_t *file)
+{
+
+	if (file == NULL || -1 == close(file -> fd))
+	{
+		crs_dbg("file NULL or file close filed\r\n");
+	}
+	crs_free( file ->info );
+	crs_free( file );
+}
 
